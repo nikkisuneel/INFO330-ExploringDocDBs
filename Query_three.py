187f81df2b947aac927ebd027178976f8f058b71
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-05-11 23:20:24 -0700

    Add files via upload

---
+++ Query_three.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+
+# Specify the database
+db = client['pokemon']
+
+# Specify the collection
+collection = db['pokemons']
+
+# Using the $regex operator to find match documents where the "abilities" field has the substring 
+# "Overgrow"
+query = {"abilities": {"$regex": ".*Overgrow.*"}}
+pokemon_with_overgrow = collection.find(query)
+
+
+for pokemon in pokemon_with_overgrow:
+    print(pokemon)
+
+
+client.close()
+
+print("Query three was successful!!")
+
