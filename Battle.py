9017efc135c03b4315c10cceaec5b985f09a6e7a
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-05-11 23:40:55 -0700

    Update Battle.py

--- Battle.py
+++ Battle.py
@@ -11,16 +11,27 @@ def fetch(pokemonid):
 def battle(pokemon1, pokemon2):
     print("Let the Pokemon battle begin! ================")
     print("It's " + pokemon1['name'] + " vs " + pokemon2['name'])
+    # Getting the score of the overall stats for each Pokemon only based on their combat stats
+    pokemon_one_score = sum(pokemon1[stat] for stat in ['hp', 'attack', 'defense', 'speed', 'sp_attack', 'sp_defense'])
+    pokemon_two_score = sum(pokemon2[stat] for stat in ['hp', 'attack', 'defense', 'speed', 'sp_attack', 'sp_defense'])
 
+    # Comparing the scores of the pokemon to see who is winner
+    if pokemon_one_score > pokemon_two_score:
+        print(pokemon1['name'] + " wins!")
+    elif pokemon_two_score > pokemon_one_score:
+        print(pokemon2['name'] + " wins!")
+    else:
+        print("Yay, well that's a tie!")
+
+    print("Stats comparison:")
     for stat in ['hp', 'attack', 'defense', 'speed', 'sp_attack', 'sp_defense']:
+        # Then comparing a Pokemon's specific stats and what they have advantages over
         if pokemon1[stat] > pokemon2[stat]:
-            print(pokemon1['name'] + " has the advantage in " + stat)
+            print(pokemon1['name'] + " is better in their ability " + stat)
         elif pokemon2[stat] > pokemon1[stat]:
-            print(pokemon2['name'] + "'s " + stat + " is superior")
-
-    winner = random.randrange(2)
-    if winner == 0: print("Battle results: " + pokemon1['name'])
-    if winner == 1: print("Battle results: " + pokemon2['name'])
+            print(pokemon2['name'] + "is better in their ability " + stat)
+        else:
+            print("The Pokemon share ability level on " + stat + " stat.")
 
 def main():
     # Fetch two pokemon from the MongoDB database
